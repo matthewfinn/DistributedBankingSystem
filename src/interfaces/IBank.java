8489736934c4b8f8b30c70ea5761250b1d61534d
Matthew Finn <m.finn11@nuigalway.ie>
2017-02-02 20:03:47 +0000

    added custom exception classes to iBank interface definition

--- src/interfaces/IBank.java
+++ src/interfaces/IBank.java
@@ -21,14 +21,26 @@ import server.Statement;
 
 public interface IBank extends Remote {
 
-	public long login(String username, String password) throws RemoteException/*, InvalidLogin*/;
+	public long login(String username, String password) throws RemoteException, InvalidLogin;
 
-	public void deposit(int accountnum, int amount, long sessionID) throws RemoteException/*, InvalidSession*/;
+	public void deposit(int accountnum, int amount, long sessionID) throws RemoteException, InvalidSession;
 
-	public void withdraw(int accountnum, int amount, long sessionID) throws RemoteException/*, InvalidSession*/;
+	public void withdraw(int accountnum, int amount, long sessionID) throws RemoteException, InvalidSession;
 
-	public int inquiry(int accountnum, long sessionID) throws RemoteException/*, InvalidSession*/;
+	public int inquiry(int accountnum, long sessionID) throws RemoteException, InvalidSession;
 
-	public Statement getStatement(Date from, Date to, long sessionID) throws RemoteException/*, InvalidSession*/;
+	public Statement getStatement(Date from, Date to, long sessionID) throws RemoteException, InvalidSession;
+
+	class InvalidLogin extends Exception{
+
+		public InvalidLogin(){};
+
+	};
+
+	class InvalidSession extends Exception{
+
+		public InvalidSession(){};
+
+	};
 
 }
--- src/server/Bank.java
+++ src/server/Bank.java
@@ -61,29 +61,30 @@ public class Bank extends UnicastRemoteObject implements IBank {
 
 	}
 	@Override
-	public long login(String username, String password) throws RemoteException/*, InvalidLogin*/ {
+	public long login(String username, String password) throws RemoteException, InvalidLogin {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 	@Override
-	public void deposit(int accountnum, int amount, long sessionID) throws RemoteException/*, InvalidSession*/ {
+	public void deposit(int accountnum, int amount, long sessionID) throws RemoteException, InvalidSession {
 		// TODO Auto-generated method stub
 
 	}
 	@Override
-	public void withdraw(int accountnum, int amount, long sessionID) throws RemoteException/*, InvalidSession*/ {
+	public void withdraw(int accountnum, int amount, long sessionID) throws RemoteException, InvalidSession {
 		// TODO Auto-generated method stub
 
 	}
 	@Override
-	public int inquiry(int accountnum, long sessionID) throws RemoteException/*, InvalidSession */{
+	public int inquiry(int accountnum, long sessionID) throws RemoteException, InvalidSession {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 	@Override
-	public Statement getStatement(Date from, Date to, long sessionID) throws RemoteException/*, InvalidSession*/ {
+	public Statement getStatement(Date from, Date to, long sessionID) throws RemoteException, InvalidSession {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 
 }
